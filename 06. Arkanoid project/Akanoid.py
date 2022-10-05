7aa600460ad12bedd6d656a167888c3904f71429
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-10-05 08:41:17 +0300

    Adding a platform that moves when left ot right keys are pressed. Making the ball to fall down from the top. Making the program to react when the two objects collide

--- 06. Arkanoid project/Akanoid.py
+++ 06. Arkanoid project/Akanoid.py
@@ -11,9 +11,16 @@ WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
 BASE_PATH = Path(__file__).resolve().parent
-BALL_WIDTH_HEIGHT = 100
+
+BALL_WIDTH_HEIGHT = 20
 MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
 MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+
+PLATFORM_WIDTH = 60
+PLATFORM_HEIGHT = 20
+PLATFORM_MAX_WIDTH = WINDOW_WIDTH - PLATFORM_WIDTH
+PLATFORM_MAX_HEIGHT = WINDOW_HEIGHT - PLATFORM_HEIGHT
+N_PIXELS_TO_MOVE = 3
 
 # 3. Initialize the world
 pygame.init()
@@ -22,12 +29,17 @@ clock = pygame.time.Clock()
 
 # 4. Load assets: image(s), sound(s), etc.
 ball_image = pygame.image.load('Images/64-Breakout-Tiles.png')
+platform_image = pygame.image.load('Images/49-Breakout-Tiles.png').convert_alpha()
+platform_image = pygame.transform.scale(platform_image, (60, 20))
 
 # 5. Initialize variables
-ball_x = random.randrange(MAX_WIDTH)
-ball_y = random.randrange(MAX_HEIGHT)
-
+ball_x = 360
+ball_y = BALL_WIDTH_HEIGHT
 ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+
+platform_x = 320
+platform_y = 460
+platform_rect = pygame.Rect(platform_x, platform_y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 
 # 6. Endless loop
 
@@ -41,18 +53,21 @@ while True:
             pygame.quit()
             sys.exit()
 
-        # See if user clicked
-        if event.type == pygame.MOUSEBUTTONUP:
-            # mouseX, mouseY = event.pos # Could do this if we needed it
-            
-            # check if click was in the rect of the ball
-            # if so, choose a random new location
-            if ball_rect.collidepoint(event.pos):
-                ball_x = random.randrange(MAX_WIDTH)
-                ball_y = random.randrange(MAX_HEIGHT)
-                ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+        # See if user pressed a key
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                platform_x -= N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_RIGHT:
+                platform_x += N_PIXELS_TO_MOVE
 
     # 8. Do any "per frame" actions
+    platform_rect = pygame.Rect(platform_x, platform_y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
+
+    ball_y += N_PIXELS_TO_MOVE
+    ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+    # Check if the ball is colliding with the platform
+    if ball_rect.colliderect(platform_rect):
+        print("Ball is touching the platform")
 
     # 9. Clear the window
     window.fill(BLACK)
@@ -60,6 +75,8 @@ while True:
     # 10. Draw all window elements
     # draw ball at position 100 across (x) and 200 down(y)
     window.blit(ball_image, (ball_x, ball_y))
+    window.blit(platform_image, (platform_x, platform_y))
+
     # 11. Update the window
     pygame.display.update()
 
